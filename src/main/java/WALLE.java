85481460b27dca15fe3fd9e03122048dfcc858a9
edwardheww <edwardhewyc@gmail.com>
2026-08-19 22:26:51 +0700

    Added echo feature & comments

--- src/main/java/WALLE.java
+++ src/main/java/WALLE.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class WALLE {
 
     public static void main(String[] args) {
+
+        // Greeting when started
         String greeting = "\n"
                 + "Hey! I'm WALLE.\n"
                 + "What can I do for you?\n"
                 + "———————————————————————\n";
         System.out.println(greeting);
+
+        // Start process of echoing user input
+        WALLE.echoUntilBye();
+
+    }
+
+    private static void echoUntilBye() {
+
+        // Listen for first user input
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // While input is not 'bye', echo input back to user
+        while (!input.equals("bye")) {
+            System.out.printf("\n   %s\n\n", input);
+            input = scanner.nextLine();
+        }
+
+        // Print goodbye message
+        System.out.println("\n  Bye bye! See you next time, I hope!\n");
+
+        // Close scanner
+        scanner.close();
+
     }
 
 }
