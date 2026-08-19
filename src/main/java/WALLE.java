a0bec2e01b677c8b7346ad7d64b8fa03d02e930a
edwardheww <edwardhewyc@gmail.com>
2026-08-19 22:10:31 +0700

    Added divider at end of greeting

--- src/main/java/WALLE.java
+++ src/main/java/WALLE.java
@@ -1,8 +1,11 @@
 public class WALLE {
+
     public static void main(String[] args) {
         String greeting = "\n"
                 + "Hey! I'm WALLE.\n"
-                + "What can I do for you?\n";
+                + "What can I do for you?\n"
+                + "———————————————————————\n";
         System.out.println(greeting);
     }
+
 }
