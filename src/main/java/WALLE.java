e5a44198efcd1eb1f69868d41f305a829fbcc391
edwardheww <edwardhewyc@gmail.com>
2026-08-19 15:39:27 +0700

    Level-0
    Renamed Duke to WALLE
    Updated String banner to simply greet user and exit

---
+++ src/main/java/WALLE.java
@@ -0,0 +1,10 @@
+public class WALLE {
+    public static void main(String[] args) {
+        String banner = "\n"
+                + "Hey! I'm WALLE.\n"
+                + "What can I do for you?\n"
+                + "\n"
+                + "Bye. Hope to see you again soon!\n";
+        System.out.println(banner);
+    }
+}
