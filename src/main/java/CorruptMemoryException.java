bb64d68310aeb1dffc29b4680fa9d973cf354c59
edwardheww <edwardhewyc@gmail.com>
2026-08-27 09:37:59 +0800

    Add CorruptMemoryException

---
+++ src/main/java/CorruptMemoryException.java
@@ -0,0 +1,7 @@
+public class CorruptMemoryException extends WALLEException {
+
+    public CorruptMemoryException() {
+        super("\n   ERROR :( Memory is corrupt...I don't remember anything...\n");
+    }
+
+}
