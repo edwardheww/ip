cc2a2ef7d7507e59ae72271dd75534dd0cf1acaf
edwardheww <edwardhewyc@gmail.com>
2026-09-02 14:58:26 +0800

    Add JavaDoc to Task base class

--- src/main/java/walle/task/Task.java
+++ src/main/java/walle/task/Task.java
@@ -1,36 +1,73 @@
 package walle.task;
 
+/**
+ * Base class for every task WALLE tracks (see {@link ToDo}, {@link Deadline},
+ * {@link Event}), holding the task's description and completion status.
+ */
 public abstract class Task {
 
     private String task;
     private boolean isDone;
 
+    /**
+     * Creates a new, not-yet-done task with the given description.
+     *
+     * @param task the task's description.
+     */
     public Task(String task) {
         this.task = task;
         this.isDone = false;
     }
 
+    /**
+     * Creates a task with the given description and completion status, e.g.
+     * when restoring a task previously loaded from storage.
+     *
+     * @param task   the task's description.
+     * @param isDone whether the task is already marked done.
+     */
     public Task(String task, boolean isDone) {
         this.task = task;
         this.isDone = isDone;
     }
 
+    /**
+     * Marks this task as done.
+     */
     public void check() {
         this.isDone = true;
     }
 
+    /**
+     * Marks this task as not done.
+     */
     public void uncheck() {
         this.isDone = false;
     }
 
+    /**
+     * Returns whether this task is marked done.
+     *
+     * @return true if the task is done, false otherwise.
+     */
     public boolean isChecked() {
         return this.isDone;
     }
 
+    /**
+     * Returns this task's description.
+     *
+     * @return the task's description.
+     */
     public String getTask() {
         return this.task;
     }
 
+    /**
+     * Returns this task's line format for the memory file, e.g. {@code T;X;task description}.
+     *
+     * @return the task's line format for the memory file.
+     */
     public abstract String getMemoryFormat();
 
     private String getCheckbox() {
